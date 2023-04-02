adde2d002fa5c780b29e7b454e1dfe17a62c5a80
BauRawson <bautistarawson@gmail.com>
2023-04-01 21:24:24 -0300

    - Add ForgeCircle

--- scenes/game.gd
+++ scenes/game.gd
@@ -3,6 +3,10 @@ extends Node2D
 
 var active_rune: String = ""
 var current_letter_index: int = -1
+var current_rune_index: int = 0
+var runes: Array = []
+var rng: RandomNumberGenerator = RandomNumberGenerator.new()
+
 onready var current_rune_label: RichTextLabel = $"%CurrentRuneRichTextLabel"
 
 export var blue: Color = Color.blue
@@ -23,6 +27,10 @@ var rune_names: Array = [
 ]
 
 
+func _ready():
+	get_new_rune_set()
+
+
 func _unhandled_input(event: InputEvent) -> void:
 	OS.set_ime_active(false)
 	if event is InputEventKey and event.is_pressed():
@@ -31,24 +39,28 @@ func _unhandled_input(event: InputEvent) -> void:
 		var key_typed = PoolByteArray([typed_event.unicode]).get_string_from_utf8()
 		print(key_typed)
 		
-		if active_rune == "":
-			find_new_active_rune(key_typed)
-		else:
-			var next_character: String = active_rune.substr(current_letter_index, 1)
+		var next_character: String = active_rune.substr(current_letter_index, 1)
+		
+		if key_typed == next_character:
+			print("success")
+			current_letter_index += 1
+			set_next_character(current_letter_index)
 			
-			if key_typed == next_character:
-				print("success")
-				current_letter_index += 1
-				set_next_character(current_letter_index)
+			if current_letter_index == active_rune.length():
+				print("word completed")
+				current_letter_index = -1
+				$CanvasLayer/Forge/ForgeCircle.get_child(current_rune_index).get_node("RuneCompleteFull").show()
 				
-				if current_letter_index == active_rune.length():
-					print("word completed")
-					current_letter_index = -1
-					active_rune = ""
-					
+				if runes.size() - 1 == current_rune_index:
+					current_rune_index = 0
 					spawn_ally()
-			else: # incorrectly typed.
-				print("not success")
+					get_new_rune_set()
+				else:
+					current_rune_index += 1
+					active_rune = runes[current_rune_index]
+					current_rune_label.bbcode_text = "[center]"+ active_rune +"[/center]"
+		else: # incorrectly typed.
+			print("not success")
 
 
 func find_new_active_rune(typed_character: String) -> void:
@@ -62,6 +74,40 @@ func find_new_active_rune(typed_character: String) -> void:
 		current_letter_index = 1
 		set_next_character(current_letter_index)
 		active_rune = prompt
+
+
+func get_new_rune_set() -> void:
+	for rune_complete_icon in $CanvasLayer/Forge/ForgeCircle.get_children():
+		rune_complete_icon.queue_free()
+	
+	runes.clear()
+	current_letter_index = -1
+	current_rune_index = 0
+	
+	rng.randomize()
+	
+	var tier: int = rng.randi_range(1, 6)
+	var icon_position_angle: float = 360 / tier
+	
+	for i in range(tier):
+		var random_rune_name: String = rune_names[randi() % rune_names.size()]
+		
+		while random_rune_name in runes:
+			random_rune_name = rune_names[randi() % rune_names.size()]
+		
+		runes.append(random_rune_name)
+		var rune_complete_icon = load("res://scenes/rune_complete_icon.tscn").instance()
+		$CanvasLayer/Forge/ForgeCircle.add_child(rune_complete_icon)
+		rune_complete_icon.rect_rotation = i * icon_position_angle
+		
+	active_rune = runes[current_rune_index]
+	current_rune_label.bbcode_text = "[center]"+ active_rune +"[/center]"
+	
+	# Get a random number, from 1 to 6
+	# Fill an array with runes, using the amount of the number
+	# Runes shouldn't repeat
+	# Set rune as 'current_rune'
+	# Fill ForgeCircle with rune_complete_icons
 
 
 func set_next_character(next_character_index: int) -> void:
